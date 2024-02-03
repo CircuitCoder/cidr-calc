22a789b2c9e0b6fbda9892a4ca3d56f864fe7886
Liu Xiaoyi <circuitcoder0@gmail.com>
2024-02-03 22:40:48 +0800

    Basic frontend

--- cidr-calc-web/src/lib.rs
+++ cidr-calc-web/src/lib.rs
@@ -1,13 +1,40 @@
 mod utils;
 
+use cidr_calculator::eval::Value;
 use wasm_bindgen::prelude::*;
+use cidr_calculator::parser::parse_single;
+use cidr_calculator::eval::eval_stmt;
+use cidr_calculator::eval::Scope;
+use cidr_calculator::eval::format;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[derive(Default)]
+pub struct EvalState {
+    scope: Scope
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, cidr-calc-web!");
+pub fn create_state() -> EvalState {
+    Default::default()
+}
+
+#[wasm_bindgen]
+pub fn print_scope(state: &EvalState) -> Vec<String> {
+    state.scope.keys().map(|e| e.to_owned()).collect()
+}
+
+#[wasm_bindgen]
+pub fn eval_input(state: &mut EvalState, input: String) -> Result<Vec<String>, String> {
+    let stmt = parse_single(&input).map_err(|e| e.to_string())?;
+    let (v, s) = eval_stmt(&stmt, state.scope.clone()).map_err(|e| e.to_string())?;
+    state.scope = s;
+
+    match v {
+        Value::Unit => {
+            Ok(vec![])
+        }
+        v => {
+            Ok(format(&v).collect())
+        }
+    }
 }
